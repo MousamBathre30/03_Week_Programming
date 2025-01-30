cf88b5b594be978c44db88cc80dd824fc75d3a60
Mousam bathre <bathremousam@gmail.com>
2025-01-30 16:37:18 +0530

    Program of merge sort

---
+++ 03_Sorting/src/problemSet/mergeSort/BookPriceSort.java
@@ -0,0 +1,50 @@
+package problemSet.mergeSort;
+
+import java.util.Arrays;
+
+public class BookPriceSort {
+
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    public static void main(String[] args) {
+        int price[] = {244,200,400,100,422,988, 733};
+
+        System.out.println("Price of the book.. -> ");
+        System.out.println(Arrays.toString(price));
+        mergeSort(price,0,price.length-1);
+
+        System.out.println("Price of the book after sort the -> ");
+        System.out.println(Arrays.toString(price));
+    }
+}
